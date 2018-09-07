7ea1a80b88953b8730d2b9adebf91be3fd57326f
ankesh sahu <ankesh0507@gmail.com>
2018-09-07 12:05:46 +0530

    Init commit

--- server/server.js
+++ server/server.js
@@ -1,12 +1,18 @@
 const path = require('path');
 const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
 const port= process.env.PORT || 3000;
 
 const publicPath = path.join(__dirname , "../public");
  var app = express();
 
+ var server = http.createServer(app);
+
  app.use(express.static(publicPath));
- app.listen(3000,()=>{
+ 
+ server.listen(3000,()=>{
      console.log(`server is up on ${port}`);
  });
  
