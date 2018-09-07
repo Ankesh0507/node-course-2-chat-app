18fc1ba1de023da6c5389e71a52aa1522c067ecf
ankesh sahu <ankesh0507@gmail.com>
2018-09-07 13:49:16 +0530

    enter

--- server/server.js
+++ server/server.js
@@ -9,10 +9,19 @@ const publicPath = path.join(__dirname , "../public");
  var app = express();
 
  var server = http.createServer(app);
+ var io = socketIO(server);
 
  app.use(express.static(publicPath));
+
+ io.on('connection',(socket)=>{
+     console.log('new user conneted');
  
+socket.on('disconnect',()=>{
+ console.log('user is disconnected');
+});
+});
+
+
  server.listen(3000,()=>{
      console.log(`server is up on ${port}`);
  });
- 
